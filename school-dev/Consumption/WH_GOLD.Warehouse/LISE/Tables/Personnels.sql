780e276b7b885045e23059b95462ab3db8fdc52c
Lee-roy <leeroyvaillant@outlook.com>
2025-12-07 21:09:19 -0400

    Committing 7 items from workspace 28e6a84a-1953-410e-8b52-272e6318afde

--- school-dev/Consumption/WH_GOLD.Warehouse/LISE/Tables/Personnels.sql
+++ school-dev/Consumption/WH_GOLD.Warehouse/LISE/Tables/Personnels.sql
@@ -4,6 +4,5 @@ CREATE TABLE [LISE].[Personnels] (
 	[Nom] varchar(100) NULL, 
 	[Prenom] varchar(100) NULL, 
 	[Nationalite] varchar(100) NULL, 
-	[SecuriteSociale] varchar(100) NULL, 
 	[Badge] int NULL
 );

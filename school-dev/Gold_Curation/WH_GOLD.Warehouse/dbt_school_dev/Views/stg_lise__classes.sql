f763b266571f94e1e73ff836fa657c85b6cc1ecc
Lee-roy <leeroyvaillant@outlook.com>
2026-04-25 22:04:55 -0400

    add new dbt_dev schema

---
+++ school-dev/Gold_Curation/WH_GOLD.Warehouse/dbt_school_dev/Views/stg_lise__classes.sql
@@ -0,0 +1,8 @@
+-- Auto Generated (Do not modify) 6D9A818D05CB07662884D477E204D560FC2F64F84531ADA3963110D7FFEB1A5E
+create view "dbt_school_dev"."stg_lise__classes" as select
+    ClasseID,
+    Classe,
+    ClasseLibelle,
+    NiveauID,
+    EtablissementID
+from "WH_GOLD"."LISE"."Classes";
---
+++ school-dev/Gold_Curation/WH_GOLD.Warehouse/dbt_school_dev/Views/stg_lise__payroll.sql
@@ -0,0 +1,15 @@
+-- Auto Generated (Do not modify) C9C9435B3DA5C018B352661C3FC4956C58E3A48CAE1310806938F58E79AC3324
+create view "dbt_school_dev"."stg_lise__payroll" as select
+    PaieID,
+    PersonnelID,
+    Nom,
+    Prenom,
+    Periode,
+    Date,
+    Montant,
+    Devise,
+    ProfessionTypeID,
+    EtablissementID,
+    ClasseID,
+    NiveauID
+from "WH_GOLD"."LISE"."Payroll";
